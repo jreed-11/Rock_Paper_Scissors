ea56987d04856fc0bb21226266f18d113fed8e6c
jreed-11 <jreedwfh@gmail.com>
2022-06-20 18:58:21 +0100

    Play round function added
    Play round function added to play 5 rounds using a for loop.

--- program.js
+++ program.js
@@ -33,3 +33,34 @@ function userPlay() {
     else (randomNumber == 3); {return "scissors"}
  
   }
+
+/* Play Round */
+
+function playRound (playerSelection, computerSelection) {
+
+   
+    if (playerSelection === computerSelection) {
+      round++;
+      console.log(`It's a draw, no one scores! and you are on round number ${round}`);
+      console.log(`The score is ${userScore} - ${computerScore}`);
+   }
+
+   else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+           (playerSelection === "scissors" && computerSelection === "paper") ||
+           (playerSelection === "paper" && computerSelection === "rock")) {
+             round++;
+             userScore++;
+             console.log(`You won. ${playerSelection} beats ${computerSelection}! and you are on round number ${round}`);
+             console.log(`The score is ${userScore} - ${computerScore}`);
+           }
+
+   else if ((computerSelection === "rock" && playerSelection === "scissors") ||
+           (computerSelection === "scissors" && playerSelection === "paper") ||
+           (computerSelection === "paper" && playerSelection === "rock")) {
+             round++;
+             computerScore++;
+             console.log(`You lost, ${computerSelection} beats ${playerSelection}! and you are on round number ${round}`);
+             console.log(`the score is ${userScore} - ${computerScore}`);              
+           } 
+ }  
+
